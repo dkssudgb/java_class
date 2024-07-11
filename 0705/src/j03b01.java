5610e9d793ea8244ab043e0122e0a7b0745f79ac
dkssudgb <115577097+dkssudgb@users.noreply.github.com>
2024-07-11 12:56:36 +0900

    Commit

--- 0705/src/j03b01.java
+++ 0705/src/j03b01.java
@@ -1,3 +1,4 @@
+import java.math.BigInteger;
 import java.util.Scanner;
 
 public class j03b01 {
@@ -15,3 +16,21 @@ public class j03b01 {
         sc.close();
     }
 }
+
+// 오버플로우 방지 : BigInteger
+//public class j03b01 {
+//    public static BigInteger factorial(int n) {
+//        if (n <= 1)
+//            return BigInteger.ONE;
+//        else
+//            return BigInteger.valueOf(n).multiply(factorial(n - 1));
+//    }
+//
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        int n = sc.nextInt();
+//        System.out.println(factorial(n));
+//        System.out.println(factorial(n).mod(BigInteger.valueOf(1_000_000_007)));
+//        sc.close();
+//    }
+//}
